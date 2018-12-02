f75adf6fcdc60cd1d73f94b8a28aa2db57c2b128
geisher09 <you@example.com>
2018-12-02 22:47:17 +0800

    Login Design

--- lib/Screens/Login/index.dart
+++ lib/Screens/Login/index.dart
@@ -27,6 +27,7 @@ class _LoginScreenState extends State<LoginScreen>{
 
 
     return new Scaffold(
+      //resizeToAvoidBottomPadding: false,
       backgroundColor: Colors.white,
       body: new Stack(
         fit: StackFit.expand,
@@ -47,98 +48,103 @@ class _LoginScreenState extends State<LoginScreen>{
               ),
             ),
           ),
-          new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Container(
-                padding: EdgeInsets.only(left: 30.0, right: 30.0),
-                child: new Card(
-                  shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(10.0)),
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 10.0),
-                    child: Column(children: <Widget>[
-                      Container(
-                        height: 150.0,
-                        width: 300.0,
-                        decoration: new BoxDecoration(
-                          image: new DecorationImage(
-                            image: new AssetImage(
-                                "assets/images/tup_gradient.png"),
-                            fit: BoxFit.cover,
+          SingleChildScrollView(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Container(
+                  height: 90.0,
+                ),
+                new Container(
+                  padding: EdgeInsets.only(left: 30.0, right: 30.0),
+                  child: new Card(
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(10.0)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: Column(children: <Widget>[
+                        Container(
+                          height: 150.0,
+                          width: 300.0,
+                          decoration: new BoxDecoration(
+                            image: new DecorationImage(
+                              image: new AssetImage(
+                                  "assets/images/tup_gradient.png"),
+                              fit: BoxFit.cover,
+                            ),
                           ),
-                        ),
-                        child: new Stack(
-                          children: <Widget>[
-                            new Positioned(
-                              left: 38.0,
-                              top: 30.0,
-                              child: new Text(
-                                "PROCURA",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: 'Montserrat',
-                                    fontSize: 30.0,
-                                    fontWeight: FontWeight.bold,
-                                    letterSpacing: 5.0
+                          child: new Stack(
+                            children: <Widget>[
+                              new Positioned(
+                                left: 38.0,
+                                top: 30.0,
+                                child: new Text(
+                                  "PROCURA",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 30.0,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 5.0
+                                  ),
                                 ),
                               ),
-                            ),
-                            new Positioned(
-                              left: 40.0,
-                              top: 75.0,
-                              child: new Text(
-                                'Better Procurement System.',
-                                style: new TextStyle(
-                                    fontSize: 15.0,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white),
+                              new Positioned(
+                                left: 40.0,
+                                top: 75.0,
+                                child: new Text(
+                                  'Better Procurement System.',
+                                  style: new TextStyle(
+                                      fontSize: 15.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
                               ),
-                            ),
-                            new Positioned(
-                              left: 70.0,
-                              top: 95.0,
-                              child: new Text(
-                                'Better Work Place.',
-                                style: new TextStyle(
-                                    fontSize: 15.0,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white),
+                              new Positioned(
+                                left: 70.0,
+                                top: 95.0,
+                                child: new Text(
+                                  'Better Work Place.',
+                                  style: new TextStyle(
+                                      fontSize: 15.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                      new FractionalTranslation(
-                        translation: Offset(0.0, -0.5),
-                        child: new Image(
-                          image: new AssetImage("assets/images/logo.png"),
-                          height: 70.0,
-                          width: 70.0,
-                          fit: BoxFit.fitHeight,
+                        new FractionalTranslation(
+                          translation: Offset(0.0, -0.5),
+                          child: new Image(
+                            image: new AssetImage("assets/images/logo.png"),
+                            height: 70.0,
+                            width: 70.0,
+                            fit: BoxFit.fitHeight,
+                          ),
                         ),
-                      ),
-                      new FractionalTranslation(
-                        translation: Offset(0.0, -0.3),
-                        child: new FormContainer(),
-                      ),
-                      new RaisedButton(
-                        elevation: 4.0,
-                        color: Colors.red,
-                        child: Text(
-                          "Log In",
-                          style: cardButtonStyle,
+                        new FractionalTranslation(
+                          translation: Offset(0.0, -0.3),
+                          child: new FormContainer(),
                         ),
-                        padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                        shape: new RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(30.0)),
-                        onPressed: () {},
-                      ),
-                    ]),
+                        new RaisedButton(
+                          elevation: 4.0,
+                          color: Colors.red,
+                          child: Text(
+                            "Log In",
+                            style: cardButtonStyle,
+                          ),
+                          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                          shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(30.0)),
+                          onPressed: () {},
+                        ),
+                      ]),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
