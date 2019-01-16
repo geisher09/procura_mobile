2533409e6121128bf0ddd5e307cef2449e0c0f7c
geisher09 <you@example.com>
2019-01-17 05:34:03 +0800

    My Profile and second screens for drawer items

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,17 @@ import 'package:procura/Screens/Login/index.dart';
 import 'Routes2.dart';
 import 'dart:async';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:wifi/wifi.dart';
 import 'package:get_ip/get_ip.dart';
 
-Future<InternetAddress> get selfIP async {
-  String ip = await Wifi.ip;
-  return InternetAddress(ip);
-}
-  final String host = "http://192.168.22.9/ProcuraMobile";
+
+
+  final String host = "http://192.168.22.8/ProcuraMobile";
 void main() async {
   Widget _defaultHome = new LoginScreen();
   final prefs = await SharedPreferences.getInstance();
-  final host2 = await selfIP;
+  String ipAdd = await GetIp.ipAddress;
   final Id = prefs.getString('id') ?? '0';
-  print(host2);
+  print(ipAdd);
   if (Id != '0') {
     _defaultHome = new Home_AdminScreen();
   }
